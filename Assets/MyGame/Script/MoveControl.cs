433afbcd33056eb96f797c8096dcfd23b17e3379
luckystar262 <trungkienlt262@gmail.com>
2025-08-26 15:33:47 +0700

    sa
    as

--- Assets/MyGame/Script/MoveControl.cs
+++ Assets/MyGame/Script/MoveControl.cs
@@ -6,11 +6,8 @@ public class MoveControl : MonoBehaviour
 {
     private Rigidbody2D rb;
     [SerializeField] float moveSpeed;
-    [SerializeField] float jumpFore;
-    [SerializeField] Transform groundCheck;
-    float rightLeft;
-    private bool isGround;
-    public LayerMask groundPlayer;
+
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -19,25 +16,15 @@ public class MoveControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundPlayer);
-        rightLeft = Input.GetAxisRaw("Horizontal");
-        if (rightLeft !=0) 
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput != 0f)
         {
-            Move(rightLeft);
+            Move(horizontalInput);
         }
-        if (isGround && Input.GetKeyDown(KeyCode.Space)) 
-        {
-            JumpFore();
-        }
-        Debug.Log(isGround);
+        
     }
     private void Move(float dir)
-    {
-        rb.velocity = new Vector2(dir * moveSpeed, rb.velocity.y);
-    }
-
-    private void JumpFore() 
-    {
-        rb.velocity = new Vector2( rb.velocity.x, jumpFore);
-    }
+        {
+            rb.velocity = new Vector2(dir * moveSpeed, rb.velocity.y);
+        }
 }
